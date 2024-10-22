921c2322d22f4e481ff17a85557fa93eaf93c24f
sagethewise <asliggiray@outlook.com>
2024-10-22 09:11:32 +0300

    master name is changed to main and valid name branch is created

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b0df2dcab2cee0a5fbb85d101e0108efb6c636d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0141ea4129243422d9be7a9e998967d1dd6ddd9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
